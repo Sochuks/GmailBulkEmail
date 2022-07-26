c7ce4c417496754937d4fb4ce313c0409a5b05cc
Sochuks <moseschukwumakajang@gmail.com>
2022-07-26 17:05:53 +0100

    change the format used for displaying the messages Added SSL to the connection process

--- script.py
+++ script.py
@@ -1,16 +1,18 @@
 import pandas as pd
 import smtplib
 
+
 e = pd.read_excel("emailList.xlsx")
 emails = e['Emails'].values
 
-mailServer = smtplib.SMTP('smtp.gmail.com' , 587)
-mailServer.starttls()
+mailServer = smtplib.SMTP_SSL('smtp.gmail.com' , 465)
+
 mailServer.login("adamconetwork@gmail.com", "rmauhwcwrcsrcvaa")
 
 subject = input("Input Email Subject \n")
 msg = input("Enter Email Message \n")
 body = "Subject: {}\n\n{}".format(subject,msg)
+# f'Subject: {subject}\n\n{msg}' Alternative format for above code
 
 for email in emails:
     try:
